58d6a44aa47a138fc1518c7cc49482f834a77e19
vonbrank <vonbrank@outlook.com>
2022-12-02 16:34:32 +0800

    feat (Config, Axios): 添加 hostname 到 API URL 的映射

--- src/Utils/Axios.ts
+++ src/Utils/Axios.ts
@@ -1,12 +1,15 @@
 import _axios, { AxiosInstance } from "axios";
+import { getServiceBaseURL } from "../config/SERVICE_BASE_API_URLS";
+
+console.log("[Axios] current hostname = ", window.location.hostname);
 
 const Axios = (baseURL: string): AxiosInstance => {
-    const instance = _axios.create({
-        baseURL: baseURL || "http://localhost:5000/api", //JSON-Server端口位置
-        timeout: 2000,
-    });
+  const instance = _axios.create({
+    baseURL: baseURL || getServiceBaseURL(), //JSON-Server端口位置
+    timeout: 2000,
+  });
 
-    return instance;
+  return instance;
 };
 
 export { Axios as axios };
